94b3f7a6e0b95092a469fe80afd97aa89aebded2
Artem <tem4ik20.1@gmail.com>
2025-05-16 23:48:35 +0300

    complete without css

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,12 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid; 
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const categoriesList = document.getElementById('categories');
+const categories = categoriesList.querySelectorAll('.item');
 
+console.log(`Numbers of  categories: ${categories.length}`);
+
+categories.forEach(category => {
+    const title = category.querySelector('h2').textContent;
+    const elements = category.querySelectorAll('li');
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${elements.length}`);
+    
+})
